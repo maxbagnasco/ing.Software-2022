224f2cee13e8b93f5c06a98c0c8b41fd60a791eb
guido <guido@blockinar.com>
2022-07-11 23:37:26 -0300

    ultimas modificaciones

--- routes/users.js
+++ routes/users.js
@@ -7,11 +7,11 @@ const getUser = (req,res,next)=>{
     const{user,password}=req.body
     const result = service.getUser(user,password);
     result ? res.json({message:`Ingreso ok de usuario ${user}`}) : 
-    res.send("user: ingenieriasoftware password: 123456");
+    res.send("Ingreso Invalido, pruebe con user ingenieriasoftware | password 123456");
   } catch (error) {
     console.log(error);
     res.json({message:"Error en user o password"})
-    // res.sendStatus(403);
+    //res.sendStatus(403);
     //el servidor entiende la solicutd, pero se niega a autroizarla.
   }
 }
@@ -29,6 +29,6 @@ const createUser = (req,res,next)=>{
     //el servidor entiende la solicutd, pero se niega a autroizarla.
   }
 }
-router.get('/login',getUser);
+router.post('/login',getUser);
 router.post('/create', createUser)
 module.exports = router;
